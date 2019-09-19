ef774513dc933060a5074ce2b6e9948adee138ea
mrvan2851 <mrvan2851@gmail.com>
2019-09-20 01:16:58 +0700

    update

--- frontend/src/views/category/create.vue
+++ frontend/src/views/category/create.vue
@@ -14,16 +14,59 @@
             <div class="row">
                 <div class="col col-4">
                     <div class="card-aside">
-                        <h2>Aside Heading</h2>
-                        <p>content</p>
+                        <h2>Category</h2>
                     </div>
                 </div>
                 <div class="col col-8">
                     <div class="card">
                         <div class="card-section">
-                            <h5>Section Heading</h5>
-                            <p>content</p>
-							<button @click="show">show</button>
+							<div class="m-b-15">
+								<label class="m-b-5">Code</label>
+								<input type="text" >
+							</div>
+							<div class="m-b-15">
+								<label class="m-b-5">Name [Vi]</label>
+								<input type="text" >
+							</div>
+							<div class="m-b-15">
+								<label class="m-b-5">Name [En]</label>
+								<input type="text" >
+							</div>
+							<div class="m-b-15">
+								<div class="box-item-image" @click="gallery">
+									<template v-if="form.primary_image">
+										<img :src="form.primary_image.path" alt="">
+									</template>
+									<template v-else>
+										<i class="icon-add-section"></i>
+									</template>
+								</div>
+							</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			<div class="row">
+                <div class="col col-4">
+                    <div class="card-aside">
+                        <h2>SEO</h2>
+                    </div>
+                </div>
+                <div class="col col-8">
+                    <div class="card">
+                        <div class="card-section">
+							<div class="m-b-15">
+								<label class="m-b-5">meta_title</label>
+								<input type="text" >
+							</div>
+							<div class="m-b-15">
+								<label class="m-b-5">meta_description</label>
+								<input type="text" >
+							</div>
+							<div class="m-b-15">
+								<label class="m-b-5">meta_keyword</label>
+								<input type="text" >
+							</div>
                         </div>
                     </div>
                 </div>
@@ -34,12 +77,24 @@
 <script>
 export default {
 	name : 'asdad',
+	data(){
+		return {
+			form :{
+				code : '',
+				vi_name : '',
+				primary_image : null
+			}		
+		}
+	},
 	methods:{
-		show(){
-			this.$gallery.show(function(image){
-				console.log(image)
+		gallery(){
+			this.$gallery.show((image)=>{
+				this.form.primary_image = image
 			})
 		}
 	}
 }
 </script>
+<style lang="scss" scoped>
+	
+</style>
